2845ef2a5f913a06d6621db4a4b6ad6374a11f36
alopex <arnout.devos@student.kuleuven.be>
2014-05-07 17:40:59 +0200

    1.3.1 af

---
+++ practicum2/poly_zeros.m
@@ -0,0 +1,14 @@
+function x = poly_zeros(n, alpha, beta, lambda)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+A = diag(alpha,0);
+
+v = beta(2:n)./lambda(2:n)
+A = A + diag(v,1);
+
+mu = 1./lambda;
+A = A + diag(mu(1:n-1),-1);
+
+x = eig(A);
+end
+

b656f31f8737d0d897d0623e43e3226fea514829
jonabeysens <jonabeysens@hotmail.com>
2014-05-12 22:27:14 +0200

    eerste vragen

---
+++ practicum2/Jona/oefening1.m
@@ -0,0 +1,6 @@
+n = 200;
+alpha = zeros(n,1);
+lambda = 2*ones(n,1);
+lambda(1) = 1;
+beta = ones(n,1);
+x = poly_zeros_jona(n,alpha,beta,lambda);
---
+++ practicum2/Jona/poly_zeros_jona.m
@@ -0,0 +1,18 @@
+function [x] = poly_zeros_jona(n,alpha,beta,lambda)
+A = zeros(n,n);
+mu = 1./lambda;
+nu = beta./lambda;
+nu = nu(2:n);
+A(1,1) = alpha(1);
+A(1,2) = mu(1);
+for i = 2:n-1
+    A(i,i) = alpha(i);
+    A(i,i-1) = nu(i-1);
+    A(i,i+1) = mu(i);
+end
+A(n,n-1) = nu(n-1);
+A(n,n) = alpha(n);
+
+x = eig(A);
+end
+

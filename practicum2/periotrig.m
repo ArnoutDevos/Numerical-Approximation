34b780116ed30caa7ac05174101b414016661368
alopex <arnout.devos@student.kuleuven.be>
2014-05-17 23:03:43 +0200

    opgave6 en opgave7 af

--- practicum2/periotrig.m
+++ practicum2/periotrig.m
@@ -1,14 +1,13 @@
 function y = periotrig(x,K,M)
-%periotrig Summary of this function goes here
-%   Detailed explanation goes here
+%periotrig Functie voor het evalueren van periodieke interpolerende en 
+%   benaderende trigonometrische veeltermen
 Y   = fft(x);
 %t   = 0:1/M:(M-1);
 [N d]   = size(x);
 
-% Coefficienten verwijderen adhv K
+% Coefficienten verwijderen aan de hand van K
 % Y_k = 0 for k = K+1,...,N/2 if K < N/2
 Y(K+2:N-K)=0;
-size(Y,1)
 
 % Y_k = X_k*M/N for k = 0,...,N/2-1
 Y(1:N/2,:) = Y(1:N/2,:)*M/N;
@@ -16,19 +15,14 @@ Y(1:N/2,:) = Y(1:N/2,:)*M/N;
 Y(N/2+1,:)=Y(N/2+1,:)*M/(N*2);
 % Y_k = 0 for k = N/2+1,... ,M/2
 Y(N/2+2:M/2+1,:)=0;
-%Y = [Y; zeros()];
+% Alleen de nodige coefficienten overhouden
 Y = Y(1:M/2+1,:);
-size(Y,1)
 
-% steunend op symmetrie
+% Steunen op de symmetrie
 %Y(M/2+1:M-1,:) = conj(Y(M/2:2,:));
 
 for k = M/2+1:M-1
     Y = [Y; conj(Y(M-k+1,:))];
 end
-size(Y,1)
-% for k = M/2+2:M %Y_k = Y_(M-k) for k = M/2+1,...,M-1
-%     Y = [Y; conj(Y(M-k+1,:))];
-% end
 y = ifft(Y,M);
 end
--- practicum2/periotrigtest.m
+++ practicum2/periotrigtest.m
@@ -1,26 +1,66 @@
-N = 256;
+N = 512;
 
 t1 = 0:1/(N-1):1;
 c = zeros(1:length(t1),1);
-for k = 1:20
+freq = 10
+for k = 1:freq
     c = c + sin(k*t1'*2*pi);
 end
 
-M = 256;
-K = 21;
+M = 512;
+K1 = 256;
+K2 = 10;
+K3 = 5;
 
 t2 = 0:1/(M-1):1;
-y = periotrig(c,K,M);
+y1 = periotrig(c,K1,M);
 
-figure(1)
-subplot(3,1,1)
+ gcf = figure(1);
+x0=10;y0=10;width=1000;height=200;
+set(gcf,'paperunits','points','paperposition',[x0,y0,width,height])
+subplot(1,2,1)
+ plot(t1,c(:,1),'b');
+subplot(1,2,2)
+fftje = abs(fft(c));
+ plot(0:2*freq-1,fftje(1:2*freq),'b');
+ print -depsc periotriga.eps;
+ 
+y2 = periotrig(c,K2,M);
+
+ gcf = figure(2);
+x0=10;y0=10;width=1000;height=200;
+set(gcf,'paperunits','points','paperposition',[x0,y0,width,height])
+subplot(1,2,1)
 hold on
-plot(t1,c(:,1),'b');
-plot(t2,y(:,1),'r');
-title('Ingevoerde en benaderende functie')
+ plot(t2,y1(:,1),'b');
+ plot(t2,y2(:,1),'r');
+ hold off
+subplot(1,2,2)
+hold on
+ fftje = abs(fft(y1));
+ plot(0:2*freq-1,fftje(1:2*freq),'b');
+ fftje = abs(fft(y2));
+ plot(0:2*freq-1,fftje(1:2*freq),'r');
+hold off
+ print -depsc periotrigb.eps;
 
-subplot(3,1,2)
-plot(abs(fft(c)),'b');
-subplot(3,1,3)
-plot(abs(fft(y)),'r');
-title('FFTs van ingevoerde en benaderende functie')
+ 
+y3 = periotrig(c,K3,M);
+
+ gcf = figure(3);
+x0=10;y0=10;width=1000;height=200;
+set(gcf,'paperunits','points','paperposition',[x0,y0,width,height])
+subplot(1,2,1)
+hold on
+ plot(t2,y1(:,1),'b');
+ plot(t2,y3(:,1),'r');
+ hold off
+subplot(1,2,2)
+hold on
+ fftje = abs(fft(y1));
+ plot(0:2*freq-1,fftje(1:2*freq),'b');
+ fftje = abs(fft(y3));
+ plot(0:2*freq-1,fftje(1:2*freq),'r');
+hold off
+ print -depsc periotrigc.eps;
+ 

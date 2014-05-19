51ca700594a6ab61696d7300c61ce124a0deb4b6
alopex <arnout.devos@student.kuleuven.be>
2014-05-19 18:09:15 +0200

    kleine aanpassingen bij opgave8

--- practicum2/clickperiotrig.m
+++ practicum2/clickperiotrig.m
@@ -1,9 +1,14 @@
 [x y] = click();
 figure
-plot(x,y);
-print -depsc infclick.eps;
+hold on
+axis([0 1 0 1]);
+plot(x,y,'bx-');
+%print -depsc infclick.eps;
+hold off
 
-z = periotrig([x' y'],5,10*length(x));
+z = periotrig([x' y'],2,100*length(x));
 figure
+hold on
+axis([0 1 0 1]);
 plot(z(:,1),z(:,2));
-print -depsc inftrig.eps;
+%print -depsc inftrig.eps;
--- practicum2/lissajous.m
+++ practicum2/lissajous.m
@@ -1,6 +1,7 @@
 t = 0:0.01:2*pi+0.1;
 A = sin(1*t);
-B = sin(2*t);
+B = sin(2*t)/2;
 figure
 plot(A,B)
+axis equal
 print -depsc lissajous.eps;
